0fe2ee2396b6fb33d5b83a55fafe7d65cf875399
AbdelrahmanAbboushi <134748594+abboushi123@users.noreply.github.com>
2024-03-31 23:30:40 +0300

    Update ShowCalendar.java
    edit print calender

--- src/main/java/eventPlaner/ShowCalendar.java
+++ src/main/java/eventPlaner/ShowCalendar.java
@@ -23,31 +23,44 @@ public class ShowCalendar {
 
         }
     }
-  public void PrintCalender(int month, int year) {
+public void PrintCalender(int month, int year) {
     month--;
     Calendar calendar = new GregorianCalendar(year, month, 1);
-    logger.info("\nCalendar for " + calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, java.util.Locale.US) + " " + year);
-    logger.info("Sun Mon Tue Wed Thu Fri Sat");
-    int firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-    StringBuilder sb = new StringBuilder();
-    for (int i = Calendar.SUNDAY; i < firstDayOfWeek; i++) {
-        sb.append("    ");
-    }
-    int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-    for (int day = 1, dayOfWeek = firstDayOfWeek; day <= daysInMonth; day++, dayOfWeek++) {
-        String currentDateString = String.format("%d-%02d-%02d", year, month + 1, day);
-        boolean isSpecialDate = Dates.stream().anyMatch(d -> sdf.format(d).equals(currentDateString));
-        if (isSpecialDate) {
-            sb.append(String.format("\u001B[31m%3d\u001B[0m ", day)); // Keeping color coding if necessary
-        } else {
-            sb.append(String.format("%3d ", day));
+
+   
+    if (logger.isInfoEnabled()) {
+        String header = String.format("\nCalendar for %s %d", calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, java.util.Locale.US), year);
+        logger.info(header);
+        logger.info("Sun Mon Tue Wed Thu Fri Sat");
+        
+        StringBuilder sb = new StringBuilder();
+        int firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        for (int i = Calendar.SUNDAY; i < firstDayOfWeek; i++) {
+            sb.append("    ");
         }
-        if (dayOfWeek == Calendar.SATURDAY) {
-            sb.append("\n");
-            dayOfWeek = Calendar.SUNDAY - 1;
+        
+        int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        for (int day = 1, dayOfWeek = firstDayOfWeek; day <= daysInMonth; day++, dayOfWeek++) {
+            String currentDateString = String.format("%d-%02d-%02d", year, month + 1, day);
+            boolean isSpecialDate = Dates.stream().anyMatch(d -> sdf.format(d).equals(currentDateString));
+            
+            
+            if (isSpecialDate) {
+              
+                sb.append(String.format("**%3d** ", day));
+            } else {
+                sb.append(String.format("%3d ", day));
+            }
+            
+            if (dayOfWeek == Calendar.SATURDAY) {
+                sb.append("\n");
+                dayOfWeek = Calendar.SUNDAY - 1;
+            }
         }
+        
+       
+        logger.info(sb.toString());
     }
-    logger.info(sb.toString());
 }
 
 
