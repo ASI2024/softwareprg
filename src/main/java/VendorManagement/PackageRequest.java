b107daaac87df85ba99b7279e15bad4f7ebddef9
AbdelrahmanAbboushi <134748594+abboushi123@users.noreply.github.com>
2024-03-31 21:53:06 +0300

    Update PackageRequest.java
    edit

--- src/main/java/VendorManagement/PackageRequest.java
+++ src/main/java/VendorManagement/PackageRequest.java
@@ -1,14 +1,8 @@
 package VendorManagement;
 
 public class PackageRequest {
-    private String requestId;
-    private String vendorId;
-    private String details;
-
-
-    public PackageRequest(String requestId, String vendorId, String details) {
-        this.requestId = requestId;
-        this.vendorId = vendorId;
-        this.details = details;
+   
+    public PackageRequest() {
+        
     }
 }
