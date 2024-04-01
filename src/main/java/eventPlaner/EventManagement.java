58b687d0dfaf53d5309792cf63785617e7744faf
AbdelrahmanAbboushi <134748594+abboushi123@users.noreply.github.com>
2024-04-01 18:01:30 +0300

    Update EventManagement.java
    edit

--- src/main/java/eventPlaner/EventManagement.java
+++ src/main/java/eventPlaner/EventManagement.java
@@ -10,27 +10,27 @@ import java.util.logging.Level;
 import java.util.Arrays;
 
 public class EventManagement {
- private static final Logger LOGGER = Logger.getLogger(EventManagement.class.getName());
-      static List<Event> eventsRequest = new ArrayList<>();
+    private static final Logger LOGGER = Logger.getLogger(EventManagement.class.getName());
+    static List<Event> eventsRequest = new ArrayList<>();
     static List<Event> eventsEdition = new ArrayList<>();
-    public static List<String> Category = new ArrayList<>(); 
+    public static List<String> Category = new ArrayList<>();
     public static boolean addFlag;
     public static boolean editDateFlag = false;
     public static boolean editTimeFlag = false;
- public static boolean editLocationFlag = false;
+    public static boolean editLocationFlag = false;
     public static boolean editThemeFlag = false;
     public static boolean editDescriptionFlag = false;
     public static boolean deletFlag = false;
     public static boolean setCategoryFlag;
     public EventManagement() {
-    //constroctor 
+        //constroctor
     }
 
     public static void AddEvent(Event event) {
         eventsRequest.add(event);
         addFlag = true;
     }
-    
+
     public static boolean hasConflict(Event event) {
         for(Event existingEvent : eventsRequest) {
             if(existingEvent.getDate().equals(event.getDate()) &&
@@ -55,27 +55,27 @@ public class EventManagement {
     }
 
 
-   public static void EditDate(int eventNumber, LocalDate d) {
-    for (Event existingEvent : eventsRequest) {
-        if (existingEvent.getEventNumber() == eventNumber) {
-            Event E = new Event(eventNumber, d, null, null, null, null, null);
-            eventsEdition.add(E);
-            editDateFlag = true;
+    public static void EditDate(int eventNumber, LocalDate d) {
+        for (Event existingEvent : eventsRequest) {
+            if (existingEvent.getEventNumber() == eventNumber) {
+                Event E = new Event(eventNumber, d, null, null, null, null, null);
+                eventsEdition.add(E);
+                editDateFlag = true;
+            }
         }
     }
-}
 
-   public static void EditTime(int eventNumber, LocalTime t) {
-    for (Event existingEvent : eventsRequest) {
-        if (existingEvent.getEventNumber() == eventNumber) {
-            Event E = new Event(eventNumber, null, t, null, null, null, null);
-            eventsEdition.add(E);
-            editTimeFlag = true;
+    public static void EditTime(int eventNumber, LocalTime t) {
+        for (Event existingEvent : eventsRequest) {
+            if (existingEvent.getEventNumber() == eventNumber) {
+                Event E = new Event(eventNumber, null, t, null, null, null, null);
+                eventsEdition.add(E);
+                editTimeFlag = true;
+            }
         }
     }
-}
 
-  public static void EditLocation(int eventNumber, String location) {
+    public static void EditLocation(int eventNumber, String location) {
         for (Event existingEvent : eventsRequest) {
             if (existingEvent.getEventNumber() == eventNumber) {
                 Event E = new Event(eventNumber, null, null, location, null, null, null);
@@ -85,48 +85,48 @@ public class EventManagement {
         }
     }
 
-  public static void EditTheme(int eventNumber, String theme) {
-    for (Event existingEvent : eventsRequest) {
-        if (existingEvent.getEventNumber() == eventNumber) {
-            Event E = new Event(eventNumber, null, null, null, theme, null, null);
-            eventsEdition.add(E);
-            editThemeFlag = true;
+    public static void EditTheme(int eventNumber, String theme) {
+        for (Event existingEvent : eventsRequest) {
+            if (existingEvent.getEventNumber() == eventNumber) {
+                Event E = new Event(eventNumber, null, null, null, theme, null, null);
+                eventsEdition.add(E);
+                editThemeFlag = true;
+            }
         }
     }
-}
 
 
-public static void EditDescription(int eventNumber, String description) {
-    for (Event existingEvent : eventsRequest) {
-        if (existingEvent.getEventNumber() == eventNumber) {
-            Event E = new Event(eventNumber, null, null, null, null, description, null);
-            eventsEdition.add(E);
-            editDescriptionFlag = true;
+    public static void EditDescription(int eventNumber, String description) {
+        for (Event existingEvent : eventsRequest) {
+            if (existingEvent.getEventNumber() == eventNumber) {
+                Event E = new Event(eventNumber, null, null, null, null, description, null);
+                eventsEdition.add(E);
+                editDescriptionFlag = true;
+            }
         }
     }
-}
 
 
     public static void DeletEvent(int eventNumber) {
-    for (Iterator<Event> iterator = eventsRequest.iterator(); iterator.hasNext(); ) {
-        Event existingEvent = iterator.next();
-        if (existingEvent.getEventNumber() == eventNumber) {
-            iterator.remove(); 
-       
-            Event E = new Event(eventNumber, null, null, null, null, null, null);
-            eventsEdition.add(E);
-            deletFlag = true;
-            break;
+        for (Iterator<Event> iterator = eventsRequest.iterator(); iterator.hasNext(); ) {
+            Event existingEvent = iterator.next();
+            if (existingEvent.getEventNumber() == eventNumber) {
+                iterator.remove();
+
+                Event E = new Event(eventNumber, null, null, null, null, null, null);
+                eventsEdition.add(E);
+                deletFlag = true;
+                break;
+            }
         }
     }
-}
 
 
-public void ShowCategory() {
+    public void ShowCategory() {
         if (Category.isEmpty()) {
             Category.addAll(Arrays.asList("Birthday", "Wedding", "Workshop", "Graduation", "Seminar"));
         }
-        
+
         for (int i = 0; i < Category.size(); i++) {
             LOGGER.log(Level.INFO, "{0}){1}", new Object[] {i + 1, Category.get(i)});
         }
@@ -142,5 +142,5 @@ public void ShowCategory() {
             }
         }
     }
- 
+
 }
